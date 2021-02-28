082c5f336ea7676e60b38ec25789bcc9a0b23295
Dmitriy Gabidullin <qwertyk2100@yandex.ru>
2021-02-28 14:34:14 -0800

    fix

--- hw3_kafka/src/main/scala/Main.scala
+++ hw3_kafka/src/main/scala/Main.scala
@@ -1,10 +1,10 @@
+import kafka.BookProducer
 import service.impl.{BookServiceImpl, CsvReaderImpl}
 
 object Main extends App {
   val csvReader = new CsvReaderImpl()
   val csvFile = "bestsellers_with_categories.csv"
-  val bookService = new BookServiceImpl(csvReader, csvFile)
-  bookService.getBooks().foreach(println)
-
+  val bookService = new BookServiceImpl()
+  BookProducer.sendMessages("books", bookService.getBooks(csvReader.read(csvFile)))
 
 }
--- hw3_kafka/src/main/scala/kafka/BookConsumer.scala
+++ hw3_kafka/src/main/scala/kafka/BookConsumer.scala
@@ -1,27 +1,69 @@
 package kafka
 
-import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.consumer.{ConsumerRecord, ConsumerRecords, KafkaConsumer, OffsetAndMetadata}
 import org.apache.kafka.common.serialization.StringDeserializer
 
-import java.time.Duration
-import java.util.Properties
+
+import java.util.{Collections, Properties}
 import scala.collection.JavaConverters.{asJavaCollectionConverter, iterableAsScalaIterableConverter}
+import scala.collection.convert.ImplicitConversions.`collection AsScalaIterable`
+import scala.collection.immutable.HashMap
 
 object BookConsumer {
 
+  //  for (TopicPartition partition : records.partitions()) {
+  //    List<ConsumerRecord<String, String>> partitionRecords = records.records(partition);
+  //    for (ConsumerRecord<String, String> record : partitionRecords) {
+  //      System.out.println(record.offset() + ": " + record.value());
+  //    }
+  //    long lastOffset = partitionRecords.get(partitionRecords.size() - 1).offset();
+  //    consumer.commitSync(Collections.singletonMap(partition, new OffsetAndMetadata(lastOffset + 1)));
+  //  }
+  //
   val props = new Properties()
-  props.put("bootstrap.servers", "localhost:29092")
+  props.put("bootstrap.servers", "192.168.99.102:29092")
   props.put("group.id", "consumer1")
 
   val consumer = new KafkaConsumer(props, new StringDeserializer, new StringDeserializer)
 
-  consumer.subscribe(List("mytopic").asJavaCollection)
+  try {
 
-  consumer
-    .poll(Duration.ofSeconds(1))
-    .asScala
-    .foreach { r => println(r.value()) }
+    consumer.subscribe(List("books").asJavaCollection)
+    while (true) {
 
-  consumer.close()
 
+      val batchSize = 5
+      val partitions = new HashMap[String, List[ConsumerRecord[String, String]]]()
+
+      val records: ConsumerRecords[String, String] = consumer.poll(100)
+      for (partition <- records.partitions().toIterator) {
+        val partitionRecords = records.records(partition)
+        partitionRecords.forEach {
+          println
+        }
+        val lastOffset = partitionRecords.get(partitionRecords.size - 1).offset
+        consumer.commitSync(Collections.singletonMap(partition, new OffsetAndMetadata(lastOffset + 1)))
+      }
+
+
+      //    ConsumerRecords<String, String> records = consumer.poll(100);
+      //    for (ConsumerRecord<String, String> record : records) {
+      //      buffer.add(record);
+      //    }
+      //    if (buffer.size() >= minBatchSize) {
+      //      insertIntoDb(buffer);
+      //      consumer.commitSync();
+      //      buffer.clear();
+      //    }
+    }
+  } finally {
+
+    consumer.close()
+
+  }
+
+  //  consumer
+  //    .poll(Duration.ofSeconds(1))
+  //    .asScala
+  //    .foreach { r => println(r.value()) }
 }
