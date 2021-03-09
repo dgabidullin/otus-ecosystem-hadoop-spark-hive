5f3aeff70fd0d17a48c1bc1b1a633d4acdd5af5f
Dmitriy Gabidullin <dmitriy.gabidullin@brandpulse.net>
2021-03-10 01:28:41 +0300

    FIX hw2

--- hw2_hdfs/src/main/scala/App.scala
+++ hw2_hdfs/src/main/scala/App.scala
@@ -1,61 +1,70 @@
-import org.apache.hadoop.fs.Path
+import org.apache.hadoop.fs.{FileStatus, Path, _}
 import org.apache.hadoop.conf._
-import org.apache.hadoop.fs._
-import java.io.{BufferedInputStream, File, FileInputStream}
+
+import java.io.BufferedInputStream
 import java.net.URI
 
 object App {
+  val ODS_PATH = "/ods"
+  val STAGE_PATH = "/stage"
+  val defaultFS = "hdfs://namenode:9000"
+  val conf = new Configuration()
+  val fs: FileSystem = FileSystem.get(new URI(defaultFS), conf)
 
   def main(args: Array[String]) {
-    val outPath = "/ods"
-    val conf = new Configuration()
-    val fs = FileSystem.get(new URI("hdfs://192.168.99.100:9000"), conf)
-    fs.listStatus(new Path("/stage")).foreach {
-      f =>
-        fs.listStatus(f.getPath).foreach(p => {
-          val out: FSDataOutputStream = fs.create(new Path("/ods" + "/part-0000.csv"))
-          val b = new Array[Byte](1024)
-          val in = fs.open(p.getPath)
-          try {
-            var numBytes = in.read(b)
-            while (numBytes > 0) {
-              out.write(b, 0, numBytes)
-              numBytes = in.read(b)
-            }
-          } finally {
-            in.close()
-          }
-        })
+    makeFolder(ODS_PATH)
+    fs.listStatus(new Path(STAGE_PATH)).foreach {
+      stageFolder => syncOds(stageFolder)
     }
-  }
 
-  def isFolderExists(folderPath: Path, fs: FileSystem): Boolean = {
-    fs.exists(folderPath)
-  }
-
-  def makeFolder(folderPath: Path, fs: FileSystem): Unit = {
-    println(s"created new folder $folderPath")
-    fs.mkdirs(folderPath)
-  }
-
-  def removeFile(file: Path, fs: FileSystem): Boolean = {
-    println(s"removed folder $file")
-    fs.delete(file, true)
-  }
-
-  def appendFile(filepath: Path, out: FSDataOutputStream, fs: FileSystem): Unit = {
-    val b = new Array[Byte](1024)
-    val in = new BufferedInputStream(fs.open(filepath))
-    try {
-      var numBytes = in.read(b)
-      while (numBytes > 0) {
-        out.write(b, 0, numBytes)
-        numBytes = in.read(b)
+    def syncOds(stageFolder: FileStatus): Unit = {
+      val readyFiles = fs.listStatus(stageFolder.getPath).filter(p => !p.getPath.getName.contains(".inprogress"))
+      if (readyFiles.length > 0) {
+        val fileName = readyFiles.head.getPath.getName
+        val outputFilePath = s"$ODS_PATH/${stageFolder.getPath.getName}/$fileName"
+        makeFile(outputFilePath)
+        val outFile = fs.append(new Path(outputFilePath))
+        for (f <- readyFiles) {
+          mergeFiles(f.getPath, outFile)
+        }
+        println(s"folder processed successfully ${stageFolder.getPath}")
+      } else {
+        println(s"not find ready files for processing ods, skipped folder ${stageFolder.getPath}")
       }
-    } finally {
-      in.close()
+    }
+
+    def mergeFiles(filepath: Path, out: FSDataOutputStream): Unit = {
+      val b = new Array[Byte](1024)
+      val in = new BufferedInputStream(fs.open(filepath))
+      try {
+        var numBytes = in.read(b)
+        while (numBytes > 0) {
+          out.write(b, 0, numBytes)
+          numBytes = in.read(b)
+        }
+      } finally {
+        in.close()
+      }
+    }
+
+    def makeFolder(folderName: String): Unit = {
+      val path = new Path(folderName)
+      if (fs.exists(path)) {
+        println(s"already exists folder $folderName")
+      } else {
+        fs.mkdirs(path)
+        println(s"created new folder $folderName")
+      }
+    }
+
+    def makeFile(fileName: String): Unit = {
+      val path = new Path(fileName)
+      if (fs.exists(path)) {
+        println(s"already exists file $fileName")
+      } else {
+        fs.createNewFile(path)
+        println(s"created new file $fileName")
+      }
     }
   }
 }
-
-case class WorkPath(from: FileStatus, to: Path)
