eb7a40b2e1122c76b38ae4c041e44ef2579419e5
Dmitriy Gabidullin <qwertyk2100@yandex.ru>
2021-03-24 20:57:48 -0700

    fix build

--- flink/flink-table-api-app/src/main/java/net/dag/App.java
+++ flink/flink-table-api-app/src/main/java/net/dag/App.java
@@ -24,28 +24,28 @@ public class App {
                 "    'format'    = 'csv'\n" +
                 ")");
 
-//        tEnv.executeSql("CREATE TABLE buy_cnt_per_hour (\n" +
-//                "    hour_of_day    BIGINT,\n" +
-//                "    buy_cnt        BIGINT\n" +
-//                ") WITH (\n" +
-//                "    'connector' = 'elasticsearch-7',\n" +
-//                "    'hosts' = 'http://elasticsearch:9200',\n" +
-//                "    'index' = 'buy_cnt_per_hour'\n" +
-//                ")");
-
-        tEnv.executeSql("CREATE TABLE buy_cnt_per_hour_mart (\n" +
+        tEnv.executeSql("CREATE TABLE buy_cnt_per_hour (\n" +
                 "    hour_of_day    BIGINT,\n" +
                 "    buy_cnt        BIGINT\n" +
                 ") WITH (\n" +
-                "  'connector'  = 'jdbc',\n" +
-                "  'url'        = 'jdbc:mysql://mysql:3306/sql-demo',\n" +
-                "  'table-name' = 'buy_cnt_per_hour_mart',\n" +
-                "  'driver'     = 'com.mysql.jdbc.Driver',\n" +
-                "  'username'   = 'sql-demo',\n" +
-                "  'password'   = 'demo-sql'\n" +
+                "    'connector' = 'elasticsearch-7',\n" +
+                "    'hosts' = 'http://elasticsearch:9200',\n" +
+                "    'index' = 'buy_cnt_per_hour'\n" +
                 ")");
-
-
+//
+//        tEnv.executeSql("CREATE TABLE buy_cnt_per_hour_mart (\n" +
+//                "    hour_of_day    BIGINT,\n" +
+//                "    buy_cnt        BIGINT\n" +
+//                ") WITH (\n" +
+//                "  'connector'  = 'jdbc',\n" +
+//                "  'url'        = 'jdbc:mysql://mysql:3306/sql-demo',\n" +
+//                "  'table-name' = 'buy_cnt_per_hour_mart',\n" +
+//                "  'driver'     = 'com.mysql.jdbc.Driver',\n" +
+//                "  'username'   = 'sql-demo',\n" +
+//                "  'password'   = 'demo-sql'\n" +
+//                ")");
+//
+//
         Table data =
                 tEnv.from("user_behavior");
 
